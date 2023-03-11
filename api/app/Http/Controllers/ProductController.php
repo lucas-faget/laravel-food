48dbdf4971607139c21d1dfe2215a6e025e8b0b8
lucas-faget <lucas.faget@laposte.net>
2023-03-11 21:01:54 +0100

    get product list by page, show product by barcode

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -4,7 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use GuzzleHttp\Client;
+
 class ProductController extends Controller
 {
-    //
+    protected static $pageSize = 20;
+
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://world.openfoodfacts.org/api/v2/',
+            'verify' => false
+        ]);
+    }
+
+    public function index($page = 1)
+    {
+        $response = $this->client->request('GET', 'search', [
+            'query' => [
+                'page_size' => self::$pageSize,
+                'page' => $page,
+            ],
+        ]);
+        
+        $products = $response->getBody()->getContents();
+
+        return $products;
+    }
+
+    public function show(string $code)
+    {
+        $response = $this->client->request('GET', "product/$code");
+
+        $product = $response->getBody()->getContents();
+
+        return $product;
+    }
+
+    public function search(string $search)
+    {
+        // TODO
+    }
 }
--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/products/{page?}', [ProductController::class, 'index']);
+Route::get('/api/products/code/{code}', [ProductController::class, 'show']);
+Route::get('/api/products/search/{search}/{page?}', [ProductController::class, 'search']);
