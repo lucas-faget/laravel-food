8f6da79e191af5b6ef36e2b3812ead1aea3d918c
lucas-faget <lucas.faget@laposte.net>
2023-03-19 22:45:41 +0100

    front: first routes

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,46 +1,15 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    import { Router, Route, Link } from "svelte-routing";
+    import Home from "./routes/Home.svelte";
+    import Products from "./routes/Products.svelte";
 </script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  
+<Router>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+    </nav>
+    
+    <Route path="/" component="{Home}" />
+    <Route path="/products" component="{Products}" />
+</Router>
