49954997724e49a9c3b969ea6b2ac985584fd343
lucas-faget <lucas.faget@laposte.net>
2023-03-21 15:36:28 +0100

    search filter

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function getProducts(page: number) {
+export async function getProducts(page: number = 1) {
     try {
         const response = await axios.get(`http://localhost:8000/api/products/${page}`);
         return response.data.products;
@@ -13,6 +13,15 @@ export async function getProduct(barcode: number) {
     try {
         const response = await axios.get(`http://localhost:8000/api/products/code/${barcode}`);
         return response.data.product;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getFilteredProducts(search: string, page: number = 1) {
+    try {
+        const response = await axios.get(`http://localhost:8000/api/products/search/${search}/${page}`);
+        return response.data.products;
     } catch (error) {
         console.error(error);
     }
--- front/src/routes/Products.svelte
+++ front/src/routes/Products.svelte
@@ -1,29 +1,67 @@
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store';
     import { getProducts } from '../api';
+    import { getFilteredProducts } from '../api';
 
-    let products = null;
+    let products = writable([]);
+    let searchTerm: string = "";
+    let isLoading: boolean = false;
 
     onMount(async () => {
-        const data = await getProducts(1);
-        products = writable([]);
-        products.set(data);
+        handleSearch();
     });
+
+    const handleSearch = async () => {
+        isLoading = true;
+        let data: any;
+        if (searchTerm === "") {
+            data = await getProducts();
+        } else {
+            data = await getFilteredProducts(searchTerm);
+        }
+        products.set(data);
+        isLoading = false;
+    }
 </script>
   
-<div>
-    {#if products}
-        {#each $products as product}
-            <div>
-                <h2>{product.product_name}</h2>
-                <p>{product.brands}</p>
-                <p>{product.code}</p>
-                <img src={product.image_url} alt={product.product_name} />
-            </div>
-        {/each}
-    {:else}
+<div class="products">
+    <br>
+    <div class="search-filter">
+        <input type="text" bind:value={searchTerm} />
+        <button on:click={handleSearch}>Search</button>
+    </div>
+
+    {#if isLoading}
         <img src="/loading-icon.gif" alt="Loading icon" />
+    {:else}
+        {#if products && $products.length !== 0}
+            {#each $products as product}
+                <div>
+                    <h2>{product.product_name}</h2>
+                    <p>{product.brands}</p>
+                    <p>{product.code}</p>
+                    <!-- <img class="product-image" src={product.image_url} alt={product.product_name} /> -->
+                </div>
+            {/each}
+        {:else}
+            <p>No product found</p>
+        {/if}
     {/if}
 </div>
+
+<style>
+    .products {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .search-filter {
+        display: flex;
+    }
+
+    .product-image {
+        width: 80px;
+    }
+</style>
   
