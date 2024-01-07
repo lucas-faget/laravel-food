b49005589673d86c477943452fea52ece605910e
lucas-faget <lucas.faget@laposte.net>
2024-01-07 15:57:25 +0100

    [Backend] Add seed user with email contact@lucas-faget.com

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // DB::table('users')->insert([
+        //     'name' => Str::random(10),
+        //     'email' => Str::random(10).'@example.com',
+        //     'password' => Hash::make('password'),
         // ]);
+
+        DB::table('users')->insert([
+            'name' => 'Lucas Faget',
+            'email' => 'contact@lucas-faget.com',
+            'password' => Hash::make('root'),
+        ]);
     }
 }
