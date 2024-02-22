29427c5e30f54b078bf955a75a14a09564f020c8
lucas-faget <lucas.faget@laposte.net>
2024-02-22 13:43:58 +0100

    Remove welcome.blade.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::prefix('/api/products')->group(function () {
     $controller = ProductController::class;
 
